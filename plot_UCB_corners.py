9fcc0d4932288bd3eb997f249cd55f6620c4c5a8
Gijs Nelemans <nelemans@astro.ru.nl>
2025-08-22 14:42:29 +0200

    added limits
    Not: not very useful at the moment, since the same limits are used for all the plots!

--- plot_UCB_corners.py
+++ plot_UCB_corners.py
@@ -19,8 +19,9 @@ bin_file = code+'_T0.hdf5'
 gal_file = code+'_Galaxy_AllDWDs.csv'
 LISA_file = code+'_Galaxy_LISA_DWDs.csv'
 weight_file = code+'_Galaxy_LISA_Candidates_Bin_Data.csv'
+limits = None
 
-opts, args = getopt.getopt(sys.argv[1:],"c:d:f:g:G:L:v:",["code=','bin_dir=","bin_file=","gal_dir=","gal_file=","LISA_file=","variation="])
+opts, args = getopt.getopt(sys.argv[1:],"c:d:f:g:G:L:v:l:",["code=','bin_dir=","bin_file=","gal_dir=","gal_file=","LISA_file=","variation=","limits="])
 
 for opt, arg in opts:
     if opt in ("-c", "--code"):
@@ -39,6 +40,9 @@ for opt, arg in opts:
         gal_file = arg
     elif opt in ("-L", "--LISA_file"):
         LISA_file = arg
+    elif opt in ("-l", "--limits"):
+        lim = arg
+        limits = tuple(float(i) for i in lim.split(","))
     elif opt in ("-w", "--weight_file"):
         weight_file = arg
     elif opt in ("-v", "--variation"):
@@ -87,39 +91,39 @@ print(N_candidates, N_LISA)
 ## plot ZAMS hexbin
 log_P = logP_from_a(ZAMS.mass1,ZAMS.mass1,ZAMS.semiMajor)
 log_tau = np.zeros(len(log_P))
-bp.hexbin_plot(ZAMS.mass1,ZAMS.mass2,log_P,log_tau,code+' @ZAMS',bins,colmap,Ngrid)
+bp.hexbin_plot(ZAMS.mass1,ZAMS.mass2,log_P,log_tau,code+' @ZAMS',bins,colmap,Ngrid,limits)
 
 ## plot ZAMS points
 mask=ZAMS.ID.isin(LISA_IDs)
-bp.point_plot(ZAMS.mass1,ZAMS.mass2,log_P,log_tau,mask,code+' @ZAMS points')
+bp.point_plot(ZAMS.mass1,ZAMS.mass2,log_P,log_tau,mask,code+' @ZAMS points',limits)
 
 ## plot WDMS hexbin
 
 log_P = logP_from_a(WDMS.mass1,WDMS.mass1,WDMS.semiMajor)
 log_tau = np.log10(WDMS.time)
-bp.hexbin_plot(WDMS.mass1,WDMS.mass2,log_P,log_tau,code+' @WDMS formation',bins,colmap,Ngrid)
+bp.hexbin_plot(WDMS.mass1,WDMS.mass2,log_P,log_tau,code+' @WDMS formation',bins,colmap,Ngrid,limits)
 
 ## plot WDMS points
 mask=WDMS.ID.isin(LISA_IDs)
-bp.point_plot(WDMS.mass1,WDMS.mass2,log_P,log_tau,mask,code+' @WDMS points')
+bp.point_plot(WDMS.mass1,WDMS.mass2,log_P,log_tau,mask,code+' @WDMS points',limits)
 
 ## plot DWD hexbin
 
 log_P = logP_from_a(DWD.mass1,DWD.mass1,DWD.semiMajor)
 log_tau = np.log10(DWD.time)
 
-bp.hexbin_plot(DWD.mass1,DWD.mass2,log_P,log_tau,code+' @DWD formation',bins,colmap,Ngrid)
+bp.hexbin_plot(DWD.mass1,DWD.mass2,log_P,log_tau,code+' @DWD formation',bins,colmap,Ngrid,limits)
 
 ## plot LISA full Galaxy
 log_P = logP_from_a(Galaxy.mass1,Galaxy.mass1,Galaxy.semiMajor)
 log_tau = np.log10(Galaxy.time)
 
-bp.hexbin_plot(Galaxy.mass1,Galaxy.mass2,log_P,log_tau,code+' Galaxy {:,} sources'.format(int(N_candidates)),bins,colmap,Ngrid)
+bp.hexbin_plot(Galaxy.mass1,Galaxy.mass2,log_P,log_tau,code+' Galaxy {:,} sources'.format(int(N_candidates)),bins,colmap,Ngrid,limits)
 
 ##bp. plot LISA detected sources
 log_P = logP_from_a(LISA_Gal.mass1,LISA_Gal.mass1,LISA_Gal.semiMajor)
 log_tau = np.log10(LISA_Gal.time)
 
-bp.hexbin_plot(LISA_Gal.mass1,LISA_Gal.mass2,log_P,log_tau,code+' Galaxy only {:,} LISA sources'.format(int(N_LISA)),bins,colmap,Ngrid)
+bp.hexbin_plot(LISA_Gal.mass1,LISA_Gal.mass2,log_P,log_tau,code+' Galaxy only {:,} LISA sources'.format(int(N_LISA)),bins,colmap,Ngrid,limits)
 
 
